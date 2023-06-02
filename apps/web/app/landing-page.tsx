3c143d0f8ccdd960ad4474c9beb761195040530e
Boushell <jamesbxv@gmail.com>
2023-06-02 00:47:26 -0400

    Little graphic portfolio

---
+++ apps/web/app/landing-page.tsx
@@ -0,0 +1,62 @@
+'use client';
+import Link from 'next/link';
+
+import { Carousel } from '@mantine/carousel';
+import { Box, Grid, Image, List, Text } from '@mantine/core';
+
+import css from './landing-page.module.scss';
+
+export function LandingPage({
+  portfolioImages,
+}: {
+  portfolioImages: string[];
+}) {
+  return (
+    <Grid>
+      <Grid.Col span={8}>
+        <Box className={css.hero} bg="cyan" c="#ececec">
+          <Text weight="bold" size={24} mb="md">
+            👋 Hey there!
+          </Text>
+          <Text size={18}>
+            I'm James Boushell, a professional full-stack developer. I've
+            created and maintain this website to showcase my passion and
+            expertise, as well as present my resume in a relevant way!
+          </Text>
+        </Box>
+        <Box className={css.portfolioContainer}>
+          <Text weight="bold" className={css.portfolioTitle}>
+            Graphic Design Portfolio
+          </Text>
+          <Carousel mx="auto" withIndicators className={css.carousel}>
+            {portfolioImages.map((imageUrl) => (
+              <Carousel.Slide key={imageUrl}>
+                <Image src={imageUrl} alt="Portfolio image" />
+              </Carousel.Slide>
+            ))}
+          </Carousel>
+        </Box>
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <Box className={css.hero} bg="#ececec">
+          <Text weight="bold" size="lg" mb="md">
+            🧑‍💻 Website Stack
+          </Text>
+          <Text weight="bold">Front End</Text>
+          <List listStyleType="none">
+            <List.Item>NextJS / React</List.Item>
+            <List.Item>Mantine UI</List.Item>
+            <List.Item>Typescript</List.Item>
+            <List.Item>SCSS</List.Item>
+          </List>
+          <br />
+          <Text weight="bold">Cloud Platform</Text>
+          <Text>Vercel</Text>
+          <br />
+          <Text weight="bold">Version Control</Text>
+          <Link href="https://github.com/Boushell/boushell.co">GitHub</Link>
+        </Box>
+      </Grid.Col>
+    </Grid>
+  );
+}
